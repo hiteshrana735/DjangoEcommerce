d08dc4f265d514c6563da836626a95b3d3862744
Asher <haarisharry007@gmail.com>
2022-04-19 15:50:33 +0530

    Added the Images of prodcuts and rendered them out

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Product(models.Model):
     name = models.CharField(max_length=250)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    # image 
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
